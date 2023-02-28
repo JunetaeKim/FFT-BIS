50727a7174a9be23d372752320aa8e4982942002
Junetae Kim <lyjune0070@gmail.com>
2023-02-28 21:45:50 +0900

    modified

--- MainModel/ModelTraining.py
+++ MainModel/ModelTraining.py
@@ -1,47 +1,27 @@
-from tensorflow_probability import distributions as tfd
-import tensorflow_probability as tfp
-
-import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import scipy.stats as stats
-from IPython.display import Image
-from scipy.stats import sem, t
-from statsmodels.tsa.stattools import adfuller 
-from matplotlib.colors import LogNorm
-
-from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score, accuracy_score, classification_report
-from sklearn.metrics import roc_curve, roc_auc_score
-import sklearn.metrics as metrics
-
-from tensorflow import keras
-from tensorflow.keras.layers import Input, Dropout, Dense,Average,Concatenate,Lambda, Permute, Activation, dot, BatchNormalization,Multiply, Concatenate,Flatten, concatenate, Add,Dot,Reshape,Conv2D, Conv1D, LSTM,TimeDistributed,Bidirectional
 import tensorflow as tf
-from tensorflow.keras.backend import  expand_dims
-
+from tensorflow import keras
 from tensorflow.keras import regularizers
-from tensorflow.keras.regularizers import l2
-from tensorflow.keras.models import Model ,load_model
-from tensorflow.keras.utils import plot_model
-from tensorflow.keras.callbacks import ModelCheckpoint,EarlyStopping
-from tensorflow.keras.optimizers import Adam
 from tensorflow.keras import backend as K,losses
+from tensorflow.keras.layers import Input, Dense, Concatenate, Lambda, Permute, Multiply, Flatten, Add, LSTM, Bidirectional
+from tensorflow.keras.backend import expand_dims
+from tensorflow.keras.regularizers import l2
+from tensorflow.keras.models import Model
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
+from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.activations import softmax
-
 
 
 class LossHistory(keras.callbacks.Callback):
     def on_train_begin(self,logs={}):
         self.losses=[]
         self.val_losses=[]
-        
     def on_epoch_end(self,batch,logs={}):
         self.losses.append(logs.get('loss'))
         self.val_losses.append(logs.get('val_loss'))
 
 
 if __name__ == "__main__":
-    
     
     # TensorFlow wizardry
     config = tf.compat.v1.ConfigProto()
@@ -65,102 +45,91 @@ if __name__ == "__main__":
     valEEG = (valEEG - minEEG)/(maxEEG-minEEG)
     
   
-    Fs = 100      # Sampling frequency
-    DimSize = 30   # End of time 몇 초
-    SignalSize=1 # Feature 수
-    MaxCensoring = 10 # Censoring을 얼마나 할것인지 defualt 10
+    Fs = 100  # Sampling frequency = 100 Hz
+    DimSize = 30  # Length of EEG = 30 s
+    SignalSize = 1  # Number of features 
     EEGmin = -1477.0
     EEGmax = 1800.0
 
     Half = int(Fs/2)
-    InputWindowSize = Fs *DimSize
+    InputWindowSize = Fs * DimSize
     DRrate = 0.5
 
     def SigProcessing (vector):
         Casted = tf.cast(vector, tf.complex64) 
         fft = tf.signal.fft(Casted)
         Scaled_fft =fft[:,:,:,:Half]
-        #Filtered_fft = tf.cast(Scaled_fft, tf.float32)
-        Abs_fft = tf.abs(Scaled_fft)*(1/(Half*2.5)) # 상대적 진폭 Relative amplitude
+        Abs_fft = tf.abs(Scaled_fft)*(1/(Half*2.5)) # Relative amplitude
         Phase_ang = tf.math.angle(Scaled_fft)*180/np.pi + K.epsilon()
-
         return [Abs_fft,Phase_ang]
 
     def TimeAxis(input_batch):
         np_constant = [np.arange(1,DimSize+1,1).reshape(1, DimSize) for i in range(SignalSize)]
         tf_constant = K.constant(np.concatenate(np_constant,axis=0))
-        #tf_constant = tf.tile(tf.expand_dims(tf_constant,1), ([1,Half,1]))
         batch_size = K.shape(input_batch)[0]
         constantVal = tf.tile(tf.expand_dims(tf_constant,0), ([batch_size,1,1]))
         return constantVal
 
 
-    ## Input side
+    # Input : 30 s of EEG sampled at 100 Hz
     InputVec = Input(shape=(InputWindowSize, SignalSize), name='Input')
     PermutedDense1 = Permute((2,1), name='Permute')(InputVec)
-    InputReStructure =Lambda(lambda x:K.reshape(x,(-1, SignalSize, DimSize, Fs)))(PermutedDense1) #Batch, element(EEG1, EEG2.....), windowID, freq, 
+    InputReStructure =Lambda(lambda x:K.reshape(x,(-1, SignalSize, DimSize, Fs)))(PermutedDense1) 
 
+    # 1. FFT layers (Fig. 3)
+  
     Abs_fft = Lambda(SigProcessing)(InputReStructure)[0]
     Phase_ang = Lambda(SigProcessing)(InputReStructure)[1]
 
-    Sig1_FFT_inp = Lambda(lambda x:x[:, 0,:,:])(Abs_fft)
+    Sig1_Amp_inp = Lambda(lambda x:x[:, 0,:,:])(Abs_fft)
     Sig1_Phase_inp = Lambda(lambda x:x[:, 0,:,:])(Phase_ang)
 
-
-    Sig1_FFT = Bidirectional(LSTM(10, dropout=DRrate, activation='softsign', return_sequences=True))(Sig1_FFT_inp, training=True)
+    # 2. LSTM layers (Fig. 4)
+    
+    Sig1_Amp = Bidirectional(LSTM(10, dropout=DRrate, activation='softsign', return_sequences=True))(Sig1_Amp_inp, training=True)
     Sig1_Phase = Bidirectional(LSTM(10, dropout=DRrate, activation='softsign', return_sequences=True))(Sig1_Phase_inp, training=True)
 
-    Sig_concat = Concatenate()([Sig1_FFT,Sig1_Phase])
+    Sig_concat = Concatenate()([Sig1_Amp,Sig1_Phase])
     Sig_concat = Bidirectional(LSTM(10, dropout=DRrate, activation='softsign', return_sequences=True))(Sig_concat, training=True)
     Sig_concat = Bidirectional(LSTM(5, dropout=DRrate, activation='softsign', return_sequences=True))(Sig_concat, training=True)
 
-    FFT_out = Bidirectional(LSTM(10, dropout=DRrate, activation='softsign', return_sequences=True))(Sig_concat, training=True)
+    Amp_out = Bidirectional(LSTM(10, dropout=DRrate, activation='softsign', return_sequences=True))(Sig_concat, training=True)
     Phase_out = Bidirectional(LSTM(10, dropout=DRrate, activation='softsign', return_sequences=True))(Sig_concat, training=True)
 
-    ## FFT Time decay
-    FFT_weibul = Dense(10, activation='relu')(FFT_out)
-    FFT_weibul = Dense(2, activation='softplus')(FFT_weibul)
-    FFT_Alpha = Lambda(lambda x:x[:,0:1,0:1])(FFT_weibul)
-    FFT_Beta = Lambda(lambda x:x[:,0:1,1:2])(FFT_weibul)
-    TimeAxisVec = (Lambda(TimeAxis)(FFT_weibul))
-    FFT_Time_Effect = 1-tf.exp(-(TimeAxisVec/FFT_Alpha)**FFT_Beta)
+    # 3. Interpretable layers (Fig. 5)
 
+    Amp_weibul = Dense(10, activation='relu')(Amp_out)
+    Amp_weibul = Dense(2, activation='softplus')(Amp_weibul)
+    Amp_Alpha = Lambda(lambda x:x[:,0:1,0:1])(Amp_weibul)
+    Amp_Beta = Lambda(lambda x:x[:,0:1,1:2])(Amp_weibul)
+    TimeAxisVec = (Lambda(TimeAxis)(Amp_weibul))
+    Amp_Time_Effect = 1-tf.exp(-(TimeAxisVec/Amp_Alpha)**Amp_Beta) # amplitude time-decayed effect
+    Amp_out = Concatenate()([Amp_out, Amp_weibul])
+    Amp_att = Dense(25, activation='elu')(Amp_out)
+    Amp_att = Dense(Half, activation='softmax')(Amp_att) # amplitude attention
 
-    ## FFT attention
-    FFT_out = Concatenate()([FFT_out, FFT_weibul])
-    FFT_att = Dense(25, activation='elu')(FFT_out)
-    FFT_att = Dense(Half, activation='softmax')(FFT_att)
-
-
-    ## FFT Context
-    FFT_end = Multiply(name='FFT_end')([Sig1_FFT_inp, Permute((2,1))(FFT_Time_Effect), FFT_att])
-    FFT_end = Lambda(lambda x:tf.reduce_sum(x, axis=(1,2), keepdims=True)+K.epsilon())(FFT_end)
-
-
-    ## Phase Time decay
     Phase_weibul = Dense(10, activation='relu')(Phase_out)
     Phase_weibul = Dense(2, activation='softplus')(Phase_weibul)
     Phase_Alpha = Lambda(lambda x:x[:,0:1,0:1])(Phase_weibul)
     Phase_Beta = Lambda(lambda x:x[:,0:1,1:2])(Phase_weibul)
     TimeAxisVec = (Lambda(TimeAxis)(Phase_weibul))
-    Phase_Time_Effect =1-tf.exp(-(TimeAxisVec/Phase_Alpha)**Phase_Beta)
-
-    ## Phase attention
+    Phase_Time_Effect =1-tf.exp(-(TimeAxisVec/Phase_Alpha)**Phase_Beta) # phase time-decayed effect
     Phase_out = Concatenate()([Phase_out, Phase_weibul])
     Phase_att = Dense(25, activation='elu')(Phase_out)
-    Phase_att = Dense(Half, activation='softmax')(Phase_att)
+    Phase_att = Dense(Half, activation='softmax')(Phase_att) # phase attention
 
-    ## FFT Context
+    # 4. BIS calculation layers (Fig. 6) 
+    
+    Amp_end = Multiply(name='Amp_end')([Sig1_Amp_inp, Permute((2,1))(Amp_Time_Effect), Amp_att])
+    Amp_end = Lambda(lambda x:tf.reduce_sum(x, axis=(1,2), keepdims=True)+K.epsilon())(Amp_end) # amplitude context
     Phase_end = Multiply(name='Phase_end')([Sig1_Phase_inp,Permute((2,1))(Phase_Time_Effect), Phase_att])
-    Phase_end = Lambda(lambda x:tf.reduce_sum(x, axis=(1,2), keepdims=True)+K.epsilon())(Phase_end)
+    Phase_end = Lambda(lambda x:tf.reduce_sum(x, axis=(1,2), keepdims=True)+K.epsilon())(Phase_end) # phase context
 
-    Out = Add()([FFT_end,Phase_end])
+    Out = Add()([Amp_end,Phase_end])
     Out = Flatten(name='Add_end')(Out)
 
     model = Model(InputVec, Out)
-    
-    print(model.summary())
-        
+          
     
     lrate = 0.001
     decay = 1e-7
@@ -169,12 +138,11 @@ if __name__ == "__main__":
 
     model.compile(loss= losses.mean_squared_error, optimizer='adam') #losses.mean_squared_error
     
-    # MODEL_SAVE_FOLDER_PATH + '{epoch:d}-{val_loss:.6f}.hdf5'
     SaveFilePath = './Logs/ModelTrain_{epoch:d}_{loss:.7f}_{val_loss:.7f}.hdf5'
     checkpoint = ModelCheckpoint(SaveFilePath,monitor=('val_loss'),verbose=0, save_best_only=True, mode='auto')
     earlystopper = EarlyStopping(monitor='val_loss', patience=2000, verbose=1)
     history = LossHistory()
-    model.fit(trEEG[:], trBIS[:], validation_data = (valEEG[:],valBIS[:]) ,shuffle=True, batch_size=12000, epochs=15000, verbose=1, callbacks=[history,earlystopper,checkpoint])
+    model.fit(trEEG[:], trBIS[:], validation_data = (valEEG[:],valBIS[:]) ,shuffle=True, batch_size=12000, epochs=10000, verbose=1, callbacks=[history,earlystopper,checkpoint])
     
     LossRes = np.concatenate([np.reshape(history.losses, (-1,1)), np.reshape(history.val_losses, (-1,1))])
     np.save('./Logs/ModelTrain_RE2LossRes.npy', LossRes)
